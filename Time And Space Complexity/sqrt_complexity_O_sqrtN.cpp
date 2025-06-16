f15e9d58f20da200dc7da6428aac4f76b226901b
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2025-06-16 10:50:09 +0600

    square complexity

--- Time And Space Complexity/sqrt_complexity_O_sqrtN.cpp
+++ Time And Space Complexity/sqrt_complexity_O_sqrtN.cpp
@@ -2,5 +2,28 @@
 using namespace std;
 
 int main() {
+
+    // Square Root Complexity O(√n)
+    int n; // complexity O(1)
+    cin >> n; // complexity O(1)
+    
+
+    
+    // find divisors of n using square root complexity O(√n)
+
+    for(int i = 1; i <= sqrt(n); i++){ // complexity O(√n
+
+        cout << i << " " << n/i << endl; // complexity O(1)
+
+    }
+
+    // find divisors of n using square root complexity O(√n)
+
+    for(int i = 1; i* i <= n; i++){ // complexity O(√n
+        if(n % i == 0){ // complexity O(1    
+            cout << i << " " << n/i << endl; // complexity O(1)
+        }
+    }
+
     return 0;
 }
