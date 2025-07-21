5c408fc5a1e69db37e29d6cd3265717ab465826c
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2025-07-21 20:13:30 +0600

    added stack

---
+++ Stack Implimentation and STL/implement_stack_using_array.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class myStack{
+    public:
+    vector<int> v;
+
+    void push(int val){
+        v.push_back(val);
+    }
+
+    void pop(){
+        v.pop_back();
+    }
+
+    int top(){
+        return v.back();
+    }
+
+    int size(){
+        return v.size();
+    }
+
+    bool empty(){
+        return v.empty();
+    }
+};
+
+int main(){
+
+    myStack st;
+    
+    int n;
+    cin >> n;
+
+    while(n--){
+        int x;
+        cin >> x;
+        st.push(x);
+    }
+
+    while(!st.empty()){
+        cout << st.top() << endl;
+        st.pop();
+    }
+
+    return 0;
+}
