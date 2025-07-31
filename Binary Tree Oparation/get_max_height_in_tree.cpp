18d929201cd24086cf234945104a8dc76b204e3e
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2025-07-31 23:31:09 +0600

    added more code

---
+++ Binary Tree Oparation/get_max_height_in_tree.cpp
@@ -0,0 +1,95 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public: 
+        int val;
+        Node* left;
+        Node* right;
+
+    Node(int val){
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+
+};
+
+Node* input_tree(){
+    int val;
+    cin >> val;
+
+    Node* root;
+
+    if(val == -1) 
+        return root = NULL;
+
+    root = new Node(val);
+
+    queue<Node*> q;
+
+    if(root) 
+        q.push(root);
+
+
+    while(!q.empty()){
+
+        // step 1: pop the front node
+        Node* p = q.front();
+        q.pop();
+
+        // step 2: input the left child of the node
+        int left_val, right_val;
+
+        cin >> left_val >> right_val;
+
+        Node* my_left, *my_right;
+
+        if(left_val == -1) 
+            my_left = NULL;
+        else
+            my_left = new Node(left_val);
+
+
+        if(right_val == -1)
+            my_right = NULL;
+        else
+            my_right = new Node(right_val);
+
+
+        p->left = my_left;
+        p->right = my_right;
+
+        // step 3: children push into the queue
+        if(p->left)
+            q.push(p->left);
+
+        if(p->right)
+            q.push(p->right);
+    }
+
+    return root;
+}
+
+int max_height(Node* root){
+    if(root == NULL)
+        return 0;
+
+    if(root->left == NULL && root->right == NULL)
+        return 1;
+
+    int left_height = max_height(root->left);
+    int right_height = max_height(root->right);
+    
+    return max(left_height, right_height) + 1;
+}
+
+
+int main(){
+
+    Node* root = input_tree();
+
+    cout << "Max Height " << max_height(root) << endl;
+   
+    return 0;
+}
