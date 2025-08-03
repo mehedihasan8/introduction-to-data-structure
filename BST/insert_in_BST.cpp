41113b56c433712ad55c85d41da0a4107afe3123
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2025-08-03 22:04:11 +0600

    added inset in BST code

---
+++ BST/insert_in_BST.cpp
@@ -0,0 +1,118 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int val;
+    Node *left;
+    Node *right;
+
+    Node(int val){
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node* input_tree(){
+    int val;
+    cin >> val;
+
+    Node* root;
+
+    if(val == -1) 
+        return root = NULL;
+
+    root = new Node(val);
+
+    queue<Node*> q;
+
+    if(root) 
+        q.push(root);
+
+
+    while(!q.empty()){
+
+        // step 1: pop the front node
+        Node* p = q.front();
+        q.pop();
+
+        // step 2: input the left child of the node
+        int left_val, right_val;
+
+        cin >> left_val >> right_val;
+
+        Node* my_left, *my_right;
+
+        if(left_val == -1) 
+            my_left = NULL;
+        else
+            my_left = new Node(left_val);
+
+
+        if(right_val == -1)
+            my_right = NULL;
+        else
+            my_right = new Node(right_val);
+
+
+        p->left = my_left;
+        p->right = my_right;
+
+        // step 3: children push into the queue
+        if(p->left)
+            q.push(p->left);
+
+        if(p->right)
+            q.push(p->right);
+    }
+
+    return root;
+}
+
+
+void level_order_print(Node* root){
+    queue<Node*> q;
+    q.push(root);
+
+    while(!q.empty()){
+        Node* f = q.front();
+        q.pop();
+
+        cout << f->val << " ";
+        
+        if(f->left)
+            q.push(f->left);
+
+        if(f->right)
+            q.push(f->right);
+    }
+}
+
+void insert(Node* &root, int val){
+    if(root == NULL){
+        root = new Node(val);
+        return;
+    }
+
+    if(val < root->val){
+        insert(root->left, val);
+    } else {
+        insert(root->right, val);
+    }
+}
+
+
+int main(){
+
+    Node* root = input_tree();
+
+    int val;
+    cin >> val;
+
+    insert(root, val);
+
+    level_order_print(root);
+
+    return 0;
+}
